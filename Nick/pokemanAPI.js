5ed0ede8e71764ee89ce219563f3db883b009cb7
ngattusohw <ngattusohw@gmail.com>
2018-05-07 16:45:56 -0400

    Adding v1 design kinda

--- Nick/pokemanAPI.js
+++ Nick/pokemanAPI.js
@@ -5,11 +5,9 @@ const getPokemonByName = function(name){
 	if(arguments.length!==1||typeof name!== 'string'){
 		throw "Error! Not a valid pokemon!"
 	}else{
-		P.getPokemonByName(name) // with Promise
-	    .then(function(response) {
+		P.getPokemonByName(name).then(function(response) {
 	      console.log(response);
-	    })
-	    .catch(function(error) {
+	    }).catch(function(error) {
 	      console.log('There was an ERROR: ', error);
 	    });
 	}
