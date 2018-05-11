0cbbbefaf96a2fad65d36c4a9f5ea07aaa0b104c
ngattusohw <ngattusohw@gmail.com>
2018-05-11 17:08:53 -0400

    Adding further untested front-back end connection for adding post

--- Nick/public/js/postHandler.js
+++ Nick/public/js/postHandler.js
@@ -1,6 +1,34 @@
 var inProgressNewPost = false;
 var inProgressNewComment = false;
 
+function postNewPost(userId){
+	var postTitle = document.getElementById("postTitle").value;
+	var postContent = document.getElementById("postContent").value;
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange=function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log("It worked!");
+			alert("Post has successfully been added!");
+			window.location.replace("/forum");
+		}else if(this.readyState == 4 && this.status == 400){
+			alert("There was a problem adding this post. Please try again, or refresh the page.");
+		}else{
+			//not done yet
+			//console.log("Hello " + this.status + " and  " + this.readyState);
+		}
+	};
+
+    xhttp.open("POST", "/forum/newPost", true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	var myData = {};
+	myData.userId = userId;
+	myData.postTitle = postTitle;
+	myData.postContent = postContent;
+	xhttp.send(JSON.stringify(myData));
+}
+
+
 function newPost(id){
 	if(!inProgressNewPost){
 		inProgressNewPost = true;
@@ -12,13 +40,16 @@ function newPost(id){
 
 		postSave.innerHTML = "Add new post!";
 		postSave.setAttribute("class", "post-entries");
+		postSave.setAttribute( "onClick", "javascript: postNewPost('userId');" );
 
 		postContent.setAttribute("class", "post-entries");
 		postContent.setAttribute("placeholder", "Post Content");
+		postContent.setAttribute("id", "postContent");
 		postContent.setAttribute("style", "height: 200px; width: 80%;");
 
 		postTitle.setAttribute("class", "post-entries");
-		postTitle.setAttribute("placeholder", "Post Title");	
+		postTitle.setAttribute("placeholder", "Post Title");
+		postTitle.setAttribute("id", "postTitle");	
 
 		postTempDiv.append(postTitle);
 		postTempDiv.append(postContent);
@@ -55,6 +86,8 @@ function postNewComment(userId,postId){
 	myData.postId = postId;
 	myData.commentValue = commentValue;
 	xhttp.send(JSON.stringify(myData));
+
+	inProgressNewComment = false;
 	
 }
 
